e4dd7b80c41513ee2a023bb05b28d8c1e759d0af
Sjouwer <sjouwer1@gmail.com>
2021-07-17 17:56:23 +0200

    Improved TpGround method

--- src/main/java/io/github/sjouwer/tputils/methods/TpForward.java
+++ src/main/java/io/github/sjouwer/tputils/methods/TpForward.java
@@ -1,6 +1,7 @@
 package io.github.sjouwer.tputils.methods;
 
 import io.github.sjouwer.tputils.config.ModConfig;
+import io.github.sjouwer.tputils.util.BlockCheck;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.client.MinecraftClient;
@@ -24,14 +25,24 @@ public class TpForward {
         HitResult hit = minecraft.cameraEntity.raycast(config.tpForwardRange(), minecraft.getTickDelta(), false);
         Vec3d vector = minecraft.player.getRotationVec(minecraft.getTickDelta());
 
-        Vec3d blockHit = hit.getPos().subtract(vector.multiply(0.05));
-        BlockPos blockPos = new BlockPos(blockHit);
+        BlockPos blockPos = new BlockPos(hit.getPos().subtract(vector.multiply(0.95)));
+        double x = blockPos.getX() + 0.5;
+        double y = blockPos.getY();
+        double z = blockPos.getZ() + 0.5;
 
         boolean isLoaded = minecraft.world.getChunkManager().isChunkLoaded(blockPos.getX() / 16, blockPos.getZ() / 16);
 
         if (isLoaded) {
             config.setPreviousLocation(minecraft.player.getPos());
-            minecraft.player.sendChatMessage(config.tpMethod() + " " + blockPos.getX() + " " + blockPos.getY() + " " + blockPos.getZ());
+
+            boolean isBottomBlockFree = !BlockCheck.canCollide(blockPos.add(0, -1, 0), !config.isLavaAllowed());
+            if (isBottomBlockFree) {
+                minecraft.player.sendChatMessage(config.tpMethod() + " " + x + " " + (y - 1) + " " + z);
+            }
+            else {
+                minecraft.player.sendChatMessage(config.tpMethod() + " " + x + " " + y + " " + z);
+            }
+
             return;
         }
 
