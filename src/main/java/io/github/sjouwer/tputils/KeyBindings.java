b4a61e6fdc8aa50912d3ce2fabcaf79a6caffab2
Sjouwer <sjouwer1@gmail.com>
2021-07-29 22:03:20 +0200

    Updated dependencies

--- src/main/java/io/github/sjouwer/tputils/KeyBindings.java
+++ src/main/java/io/github/sjouwer/tputils/KeyBindings.java
@@ -3,7 +3,7 @@ package io.github.sjouwer.tputils;
 import io.github.sjouwer.tputils.methods.*;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
