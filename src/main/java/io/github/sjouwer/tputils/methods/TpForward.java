fff36325fbd69fb0deb6e58ea88a08e19c692af5
Sjouwer <sjouwer1@gmail.com>
2021-08-03 16:52:08 +0200

    Improved TpForward method

--- src/main/java/io/github/sjouwer/tputils/methods/TpForward.java
+++ src/main/java/io/github/sjouwer/tputils/methods/TpForward.java
@@ -1,7 +1,7 @@
 package io.github.sjouwer.tputils.methods;
 
 import io.github.sjouwer.tputils.config.ModConfig;
-import io.github.sjouwer.tputils.util.BlockCheck;
+import io.github.sjouwer.tputils.util.*;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.client.MinecraftClient;
@@ -10,6 +10,7 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 
 import static net.minecraft.text.Style.EMPTY;
 
@@ -22,32 +23,54 @@ public class TpForward {
     }
 
     public void tpForward() {
-        HitResult hit = minecraft.cameraEntity.raycast(config.tpForwardRange(), minecraft.getTickDelta(), false);
-        Vec3d vector = minecraft.player.getRotationVec(minecraft.getTickDelta());
+        BlockPos pos = findOpenSpot(castRay());
 
-        BlockPos blockPos = new BlockPos(hit.getPos().subtract(vector.multiply(0.95)));
-        double x = blockPos.getX() + 0.5;
-        double y = blockPos.getY();
-        double z = blockPos.getZ() + 0.5;
-
-        boolean isLoaded = minecraft.world.getChunkManager().isChunkLoaded(blockPos.getX() / 16, blockPos.getZ() / 16);
-
-        if (isLoaded) {
-            config.setPreviousLocation(minecraft.player.getPos());
-
-            boolean isBottomBlockFree = !BlockCheck.canCollide(blockPos.add(0, -1, 0), config);
-            if (isBottomBlockFree) {
-                minecraft.player.sendChatMessage(config.tpMethod() + " " + x + " " + (y - 1) + " " + z);
+        BaseText message;
+        if (pos != null) {
+            BlockPos playerPos = new BlockPos(minecraft.player.getPos());
+            if (!pos.equals(playerPos)) {
+                Teleport.teleportPlayer(pos, config);
+                return;
             }
-            else {
-                minecraft.player.sendChatMessage(config.tpMethod() + " " + x + " " + y + " " + z);
-            }
-
-            return;
+            message = new TranslatableText("text.tp_utils.message.cantMoveForward");
+        }
+        else {
+            message = new TranslatableText("text.tp_utils.message.obstructed");
         }
 
-        BaseText message = new TranslatableText("text.tp_utils.message.chunkNotLoaded");
         message.setStyle(EMPTY.withColor(Formatting.DARK_RED));
         minecraft.player.sendMessage(message, false);
+    }
+
+    private HitResult castRay() {
+        Vec3d vector = minecraft.cameraEntity.getRotationVec(minecraft.getTickDelta());
+        Vec3d rayStart = minecraft.cameraEntity.getEyePos();
+        Vec3d rayEnd = rayStart.add(vector.multiply(config.tpForwardRange()));
+        return minecraft.world.raycast(new RaycastContext(rayStart, rayEnd, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, minecraft.player));
+    }
+
+    private BlockPos findOpenSpot(HitResult hit) {
+        double distance = minecraft.cameraEntity.getEyePos().distanceTo(hit.getPos());
+
+        for (int i = 0; i < distance * 8; i++) {
+            Vec3d vector = minecraft.cameraEntity.getRotationVec(minecraft.getTickDelta());
+            BlockPos pos = new BlockPos(hit.getPos().add(vector.multiply(-0.125 * i)));
+
+            boolean foundObstacle = BlockCheck.canCollide(pos, config);
+            boolean isLoaded = minecraft.world.getChunkManager().isChunkLoaded(pos.getX() / 16, pos.getZ() / 16);
+
+            if (isLoaded && !foundObstacle && (!config.isBedrockLimitSet() || pos.getY() > minecraft.world.getBottomY())) {
+                boolean isBottomBlockFree = !BlockCheck.canCollide(pos.add(0, -1, 0), config);
+                boolean isTopBlockFree = !BlockCheck.canCollide(pos.add(0,1,0), config);
+
+                if (isBottomBlockFree) {
+                    return new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ());
+                }
+                else if (isTopBlockFree || config.isCrawlingAllowed()) {
+                    return pos;
+                }
+            }
+        }
+        return null;
     }
 }
